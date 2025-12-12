04f1ca373ff0bff025f88507ded7540138c8764c
wildwonderwinsto <wildwonderwinston@gmail.com>
2025-12-11 22:27:24 -0600

    Simplify to Gateway only

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
-
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import WinstonsGateway from './Gateway'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <WinstonsGateway />
   </React.StrictMode>,
 )
